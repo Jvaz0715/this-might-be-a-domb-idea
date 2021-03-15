5dd665e9a393bf13c61c9d2a856976dde1b1d6de
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-15 15:46:46 -0400

    Does not update to live server

---
+++ main.js
@@ -0,0 +1,7 @@
+//const paragraph = document.querySelector('p');
+
+//paragraph.style.color = 'light blue';
+//console.log(paragraph);
+
+const heading = document.querySelector('h1');
+heading.style.fontSize = '100px';
