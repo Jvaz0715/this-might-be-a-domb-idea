34d0ad2f7f56fe1a6d882fa5bf53a77f40bd53f1
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-15 15:58:54 -0400

    Solves up to change image to this image

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ const heading = document.querySelector('h1');
 heading.style.fontSize = '48px';
 
 paragraph.innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+const thirteenthItem = document.querySelector('#item-13');
+
+thirteenthItem.style.opacity = "50%";
+
+const thirdItem = document.querySelector('#item-3');
+
+thirdItem.innerHTML = "Hi!"
+
+const image = document.querySelector('img');
+
+image.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg"
